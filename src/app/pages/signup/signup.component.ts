0c255fb69bc2c7e12f5869260637fcf1bd20396f
ginying <tunyaporn.subsakorn@gmail.com>
2024-04-18 01:18:52 +0700

    Add validation for sign up

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,6 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 
@@ -12,21 +17,52 @@ import { AuthService } from '../../services/auth.service';
   styleUrl: './signup.component.scss',
 })
 export class SignupComponent {
-  signUpFormGroup = new FormGroup({
-    email: new FormControl(),
-    name: new FormControl(),
-    password: new FormControl(),
-    confirmPassword: new FormControl(),
-  });
+  signUpFormGroup: any;
 
   constructor(private router: Router, private authService: AuthService) {}
 
+  ngOnInit() {
+    this.signUpFormGroup = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      name: new FormControl('', [
+        Validators.required,
+        Validators.pattern('[a-zA-Z ]*'),
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(5),
+      ]),
+      confirmPassword: new FormControl('', [
+        Validators.required,
+        Validators.minLength(5),
+      ]),
+    });
+  }
+
   onSubmit() {
-    this.authService
-      .postSignUp(this.signUpFormGroup.value)
-      .subscribe((data) => {
-        console.log(data);
-        this.router.navigate(['/login']);
-      });
+    if (this.signUpFormGroup.valid) {
+      this.authService
+        .postSignUp(this.signUpFormGroup.value)
+        .subscribe((data) => {
+          console.log(data);
+          this.router.navigate(['/login']);
+        });
+    }
+  }
+
+  get email() {
+    return this.signUpFormGroup.get('email');
+  }
+
+  get name() {
+    return this.signUpFormGroup.get('name');
+  }
+
+  get password() {
+    return this.signUpFormGroup.get('password');
+  }
+
+  get confirmPassword() {
+    return this.signUpFormGroup.get('confirmPassword');
   }
 }
